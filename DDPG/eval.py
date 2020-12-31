6771875818dbe3da7b888ef21054dcdfc2f35e0b
Programmer <info2016frei@gmail.com>
2020-12-31 16:44:10 +0100

    checkstyle

--- DDPG/eval.py
+++ DDPG/eval.py
@@ -1,12 +1,12 @@
 import os
 import sys
 import argparse
-   
-  
+
+
 def main(args):
     counter = 1
     name = args.locexp
-    for s in [1, 2, 3 ,4]:
+    for s in [1, 2, 3, 4]:
         print("Round {}".format(counter))
         os.system(f'python3 ./main.py \
                 --locexp {name} \
@@ -15,6 +15,5 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Hyperparameter search')
-    parser.add_argument('--locexp', type=str) 
+    parser.add_argument('--locexp', type=str)
     main(parser.parse_args())
-
